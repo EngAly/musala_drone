38267b4ac066ed8104dea60f91c21572d2d81892
Aly Ahmed <aly.ahmed@isoft.ae>
2022-10-29 11:04:48 +0200

    Configure generic exception handler

---
+++ src/main/java/com/musala/drones/configuration/ControllerException.java
@@ -0,0 +1,10 @@
+package com.musala.drones.configuration;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerException extends ResponseEntityExceptionHandler {
+
+
+}
