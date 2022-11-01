6c4420f5895619525f17925019f80a39702dd39e
Aly Ahmed <aly.ahmed@isoft.ae>
2022-11-01 22:30:59 +0200

    Add ping api with current date to check application status

--- src/main/java/com/musala/drones/controller/DroneController.java
+++ src/main/java/com/musala/drones/controller/DroneController.java
@@ -3,9 +3,14 @@ package com.musala.drones.controller;
 import com.musala.drones.dto.LoadDroneDto;
 import com.musala.drones.entity.Drone;
 import com.musala.drones.service.DroneService;
+import com.musala.drones.util.DroneConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/drone")
@@ -42,6 +47,11 @@ public class DroneController extends GenericController {
     @GetMapping("/available")
     public ResponseEntity<?> getAvailableDrones() {
         return getResponse(droneService.getAvailableDrones());
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<?> ping() {
+        return getResponse(DateTimeFormatter.ofPattern(DroneConstants.DATE_PATTERN).format(ZonedDateTime.now()));
     }
 
 
--- src/main/java/com/musala/drones/util/DroneConstants.java
+++ src/main/java/com/musala/drones/util/DroneConstants.java
@@ -5,4 +5,6 @@ public class DroneConstants {
     final public static int DRONE_BATTERY_MIN_CAPACITY = 25;
     final public static int DRONE_WEIGHT_LIMIT = 500;
     final public static long DRONE_SCHEDULE_RATE = 10000;
+    final public static String DATE_PATTERN = "yyyy-MM-dd hh:mm:ss";
+
 }
